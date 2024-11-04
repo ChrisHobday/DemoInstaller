a07836c8ecc0eedfe5b6ced351c6021ae4949787
Chris Hobday <Hobday.Chris@ProtonMail.com>
2024-11-04 13:21:26 -0500

    Remove Registry files from sources (they will be added to /app/share/Registry in the Flatpak manifest instead)

--- build/Desktop-Debug/DemoInstaller/Scripts/WineSetup.sh
+++ build/Desktop-Debug/DemoInstaller/Scripts/WineSetup.sh
@@ -1,24 +1,12 @@
 #!/bin/bash
-pwd
-
-ls
-
 echo "Initializing Wine prefix"
 wineboot -i
 
-pwd
-
-ls
-
 echo "Updating Windows registry with Risk paths"
-wine regedit /Registry/Risk.reg
+wine regedit Registry/Risk.reg
 
 echo "Updating Windows registry with Wine graphics keys"
-wine regedit /Registry/WineGraphics.reg
-
-pwd
-
-ls
+wine regedit Registry/WineGraphics.reg
 
 echo "Waiting for registry updates to be applied"
 sleep 3
