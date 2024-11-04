5519bac04fadf334787bd2f44190d47ab31f2049
Chris Hobday <Hobday.Chris@ProtonMail.com>
2024-11-04 02:31:54 -0500

    Change target binary name to DemoInstaller

--- build/Desktop-Debug/DemoInstaller/Scripts/WineSetup.sh
+++ build/Desktop-Debug/DemoInstaller/Scripts/WineSetup.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
+pwd
+
+ls
+
 echo "Initializing Wine prefix"
 wineboot -i
+
+pwd
+
+ls
 
 echo "Updating Windows registry with Risk paths"
 wine regedit /Registry/Risk.reg
 
 echo "Updating Windows registry with Wine graphics keys"
 wine regedit /Registry/WineGraphics.reg
+
+pwd
+
+ls
 
 echo "Waiting for registry updates to be applied"
 sleep 3
